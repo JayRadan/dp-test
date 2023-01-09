fb390e0d3bfbeab796a1b50c299fae75b870b824
Jay Radan <jacob.radan2@gmail.com>
2023-01-09 12:53:54 +0100

    hash router

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import TodoListContext from './TodoListContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <BrowserRouter>
+ <HashRouter>
  <TodoListContext>
   
  <App />
 
  </TodoListContext>
    
-    </BrowserRouter>
+    </HashRouter>
 );
 
